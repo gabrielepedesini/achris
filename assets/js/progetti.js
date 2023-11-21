9b873dfd67677aa59d891ab6ffd1bb955c8bee2c
Gabriele Pedesini <125191834+gabrielepedesini@users.noreply.github.com>
2023-11-21 17:50:04 +0100

    Add files via upload

--- assets/js/progetti.js
+++ assets/js/progetti.js
@@ -131,10 +131,32 @@ setTimeout(adBlockCheck, 5000);
 
 /* navbar */
 
+let navbarStatus = false;
+var hamburger = document.querySelector('.hamburger');
+
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
+
+  toggleHamburger();
+  document.getElementById("myNav").style.width = "100%";
+  navbarStatus = true;
+}
   
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+function closeNav() {
+  
+  toggleHamburger();
+  document.getElementById("myNav").style.width = "0%";
+  navbarStatus = false;
+}
+
+function toggleHamburger() {
+  hamburger.classList.toggle('is-active');
+}
+
+hamburger.addEventListener("click", function() {
+
+  if(navbarStatus) {
+    closeNav();
+  } else {
+    openNav();
   }
+})
--- assets/js/script.js
+++ assets/js/script.js
@@ -128,13 +128,35 @@ setTimeout(adBlockCheck, 5000);
 
 /* navbar */
 
+let navbarStatus = false;
+var hamburger = document.querySelector('.hamburger');
+
 function openNav() {
+
+  toggleHamburger();
   document.getElementById("myNav").style.width = "100%";
+  navbarStatus = true;
 }
   
 function closeNav() {
+  
+  toggleHamburger();
   document.getElementById("myNav").style.width = "0%";
+  navbarStatus = false;
 }
+
+function toggleHamburger() {
+  hamburger.classList.toggle('is-active');
+}
+
+hamburger.addEventListener("click", function() {
+
+  if(navbarStatus) {
+    closeNav();
+  } else {
+    openNav();
+  }
+})
 
 
 /* download */
