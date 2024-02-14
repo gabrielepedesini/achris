04d235cacec0eeedfa92d711a5199953e0eb7537
Gabriele Pedesini <125191834+gabrielepedesini@users.noreply.github.com>
2024-02-14 17:05:45 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,16 @@
 
 var preloaderComplete = false;
 
-var loader = document.getElementById('logo-intero');
-var loadercont = document.getElementById('loader-cont');
-var slideup = document.getElementById('s-up');
-var slidedown = document.getElementById('s-down');
-
+var loader = document.getElementById("logo-intero");
+var loadercont = document.getElementById("loader-cont");
+var slideup = document.getElementById("s-up");
+var slidedown = document.getElementById("s-down");
 
 function detectLanguageAndRedirect() {
   // Show the loader before making the AJAX request.
   $(loader).removeClass("complete-animation").removeClass("complete");
 
-  $.get("https://ipinfo.io?token=382b4794128a39", function(response) {
+  $.get("https://ipinfo.io?token=382b4794128a39", function (response) {
     // La risposta contiene i dati sull'IP, inclusa la nazione.
     var country = response.country;
 
@@ -20,13 +19,22 @@ function detectLanguageAndRedirect() {
     var currentLanguage = window.location.pathname.split("/")[1]; // Assumes the language code is the first part of the path (e.g., "en" in "https://example.com/en/index.html")
 
     // Check if the user is already on the correct language version
-    if (country === "IT" && (currentLanguage === "it" || currentLanguage === "" || currentLanguage === "index.html")) {
+    if (
+      country === "IT" &&
+      (currentLanguage === "it" ||
+        currentLanguage === "" ||
+        currentLanguage === "index.html")
+    ) {
       // User is already on the Italian version, no need to redirect
       handleCompleteAnimation();
     } else if (country === "JP" && currentLanguage === "ja") {
       // User is already on the Japanese version, no need to redirect
       handleCompleteAnimation();
-    } else if (country !== "IT" && country !== "JP" && currentLanguage === "en") {
+    } else if (
+      country !== "IT" &&
+      country !== "JP" &&
+      currentLanguage === "en"
+    ) {
       // User is on the English version, no need to redirect
       handleCompleteAnimation();
     } else {
@@ -46,81 +54,71 @@ function detectLanguageAndRedirect() {
 }
 
 function handleCompleteAnimation() {
-  
   preloaderComplete = true;
 
-  setTimeout(function() {
+  setTimeout(function () {
     $(loader).addClass("complete-animation");
   }, 500);
-  setTimeout(function() {
+  setTimeout(function () {
     $(loader).addClass("complete");
   }, 1000);
-  setTimeout(function() {
+  setTimeout(function () {
     $(slideup).addClass("complete-up-animation");
   }, 1000);
-  setTimeout(function() {
+  setTimeout(function () {
     $(slidedown).addClass("complete-down-animation");
   }, 1000);
-  setTimeout(function() {
+  setTimeout(function () {
     $(slideup).addClass("complete");
   }, 2000);
-  setTimeout(function() {
+  setTimeout(function () {
     $(slidedown).addClass("complete");
   }, 2000);
-  setTimeout(function() {
+  setTimeout(function () {
     $(loadercont).addClass("complete");
   }, 2000);
 }
 
 function cookieExists(name) {
-  const cookies = document.cookie.split('; ');
+  const cookies = document.cookie.split("; ");
   for (const cookie of cookies) {
-      const [cookieName, cookieValue] = cookie.split('=');
-      if (cookieName === name) {
-          return true;
-      }
+    const [cookieName, cookieValue] = cookie.split("=");
+    if (cookieName === name) {
+      return true;
+    }
   }
   return false;
 }
 
 if (cookieExists("lang-cookie")) {
-  
   handleCompleteAnimation();
-
 } else {
-  
-  $(document).ready(function() {
+  $(document).ready(function () {
     detectLanguageAndRedirect();
   });
-
 }
 
 /* ad block checker */
 
 function adBlockCheck() {
-  
   let adBlockDetected = false;
-  
-  if(!preloaderComplete) {
 
-    var testAd = document.createElement('div');
-    testAd.innerHTML = '&nbsp;';
-    testAd.className = 'adblock-test';
+  if (!preloaderComplete) {
+    var testAd = document.createElement("div");
+    testAd.innerHTML = "&nbsp;";
+    testAd.className = "adblock-test";
     document.body.appendChild(testAd);
 
     if (testAd.offsetHeight === 0) {
-        adBlockDetected = true;
+      adBlockDetected = true;
     }
     testAd.remove();
 
-
-    if(!adBlockDetected) {
-      
-      adBlockContainer = document.querySelector('.container-adblock');
+    if (!adBlockDetected) {
+      adBlockContainer = document.querySelector(".container-adblock");
 
       adBlockContainer.style.display = "flex";
     }
-
   }
 }
 
@@ -129,41 +127,37 @@ setTimeout(adBlockCheck, 5000);
 /* navbar */
 
 let navbarStatus = false;
-var hamburger = document.querySelector('.hamburger');
+var hamburger = document.querySelector(".hamburger");
 
 function openNav() {
-
   toggleHamburger();
   document.getElementById("myNav").style.width = "100%";
   navbarStatus = true;
 }
-  
+
 function closeNav() {
-  
   toggleHamburger();
   document.getElementById("myNav").style.width = "0%";
   navbarStatus = false;
 }
 
 function toggleHamburger() {
-  hamburger.classList.toggle('is-active');
+  hamburger.classList.toggle("is-active");
 }
 
-hamburger.addEventListener("click", function() {
-
-  if(navbarStatus) {
+hamburger.addEventListener("click", function () {
+  if (navbarStatus) {
     closeNav();
   } else {
     openNav();
   }
-})
-
+});
 
 /* download */
 
-var downOpen = document.getElementById('download-open');
-var downClose = document.getElementById('download-close');
-var modal = document.getElementById('modal');
+var downOpen = document.getElementById("download-open");
+var downClose = document.getElementById("download-close");
+var modal = document.getElementById("modal");
 
 function openModal() {
   modal.style.display = "flex";
@@ -213,10 +207,10 @@ downloadButtons.forEach(function (button) {
 
 // Function to check if the "cookie-consent" cookie is set to true
 function hasCookieConsent() {
-  const cookies = document.cookie.split(';');
+  const cookies = document.cookie.split(";");
   for (const cookie of cookies) {
-    const [name, value] = cookie.trim().split('=');
-    if (name === 'cookie-consent' && value === 'true') {
+    const [name, value] = cookie.trim().split("=");
+    if (name === "cookie-consent" && value === "true") {
       return true;
     }
   }
@@ -228,34 +222,35 @@ function setFlagCookie() {
   const thirtyDaysFromNow = new Date();
   thirtyDaysFromNow.setDate(thirtyDaysFromNow.getDate() + 30);
 
-  document.cookie = 'flag-cookie=true; expires=' + thirtyDaysFromNow.toUTCString() + '; path=/';
+  document.cookie =
+    "flag-cookie=true; expires=" + thirtyDaysFromNow.toUTCString() + "; path=/";
 }
 
 // Function to hide the checkbox container
 function hideCheckboxContainer() {
-  const checkboxContainer = document.getElementById('checkbox-container');
+  const checkboxContainer = document.getElementById("checkbox-container");
   if (checkboxContainer) {
-    checkboxContainer.style.display = 'none';
+    checkboxContainer.style.display = "none";
     downloadButtons.forEach(function (button) {
       button.classList.remove("disabled");
-    })
+    });
   }
 }
 
 // Check if the user has previously accepted cookies
 if (hasCookieConsent()) {
   // Hide the checkbox container if "flag-cookie" is true
-  if (document.cookie.includes('flag-cookie=true')) {
+  if (document.cookie.includes("flag-cookie=true")) {
     hideCheckboxContainer();
-  }  
+  }
 }
 
 if (masterCheckbox) {
-  masterCheckbox.addEventListener('change', function () {
+  masterCheckbox.addEventListener("change", function () {
     if (this.checked) {
       if (hasCookieConsent()) {
         setFlagCookie();
-        console.log('Checkbox is checked'); 
+        console.log("Checkbox is checked");
       }
     }
   });
@@ -263,14 +258,19 @@ if (masterCheckbox) {
 
 /* skills */
 
-var offsetTop = $('#skill-alarm').offset().top;
-$(window).scroll(function() {
-var height = $(window).height();
-if($(window).scrollTop()+height > offsetTop) {
-    jQuery('.skillbar').each(function(){
-      jQuery(this).find('.skillbar-bar').animate({
-        width:jQuery(this).attr('data-percent')
-      }, 1200);
+var offsetTop = $("#skill-alarm").offset().top;
+$(window).scroll(function () {
+  var height = $(window).height();
+  if ($(window).scrollTop() + height > offsetTop) {
+    jQuery(".skillbar").each(function () {
+      jQuery(this)
+        .find(".skillbar-bar")
+        .animate(
+          {
+            width: jQuery(this).attr("data-percent"),
+          },
+          1200
+        );
     });
   }
 });
@@ -278,7 +278,7 @@ if($(window).scrollTop()+height > offsetTop) {
 /* counters */
 
 function handleIntersection(entries, observer) {
-  entries.forEach(entry => {
+  entries.forEach((entry) => {
     if (entry.isIntersecting) {
       counterUp();
       observer.disconnect();
@@ -286,10 +286,10 @@ function handleIntersection(entries, observer) {
   });
 }
 
-const targetElement = document.querySelector('.container-counters');
+const targetElement = document.querySelector(".container-counters");
 const options = {
   root: null,
-  rootMargin: '0px',
+  rootMargin: "0px",
   threshold: 0,
 };
 
@@ -298,10 +298,10 @@ const observer = new IntersectionObserver(handleIntersection, options);
 observer.observe(targetElement);
 
 function counterUp() {
-  console.log('Element is visible!');
-  const counterNumbers = document.querySelectorAll('.num');
+  console.log("Element is visible!");
+  const counterNumbers = document.querySelectorAll(".num");
 
-  counterNumbers.forEach(num => {
+  counterNumbers.forEach((num) => {
     const meta = parseInt(num.innerHTML);
     num.innerHTML = 0;
     let iteration = 0;
@@ -318,29 +318,32 @@ function counterUp() {
     };
 
     increment();
-    
   });
 }
 
 /* data */
 
-window.onload = function() {
+window.onload = function () {
   // La data di nascita (17 giugno) nel formato mm/gg/aaaa
   const birthday = "06/17/2003";
 
   // Funzione per calcolare l'età in base alla data di nascita
   function calculateAge(birthDate) {
     const today = new Date();
-    const birthDateFormatted = new Date(birthDate.replace("YYYY", today.getFullYear()));
-    
+    const birthDateFormatted = new Date(
+      birthDate.replace("YYYY", today.getFullYear())
+    );
+
     let age = today.getFullYear() - birthDateFormatted.getFullYear();
-    const isBirthdayPassed = today.getMonth() > birthDateFormatted.getMonth() ||
-                            (today.getMonth() === birthDateFormatted.getMonth() && today.getDate() >= birthDateFormatted.getDate());
-    
+    const isBirthdayPassed =
+      today.getMonth() > birthDateFormatted.getMonth() ||
+      (today.getMonth() === birthDateFormatted.getMonth() &&
+        today.getDate() >= birthDateFormatted.getDate());
+
     if (!isBirthdayPassed) {
       age -= 1;
     }
-    
+
     return age;
   }
 
@@ -348,7 +351,7 @@ window.onload = function() {
   function updateAge() {
     const ageElement = document.getElementById("age");
     const age = calculateAge(birthday);
-    ageElement.textContent =  age; 
+    ageElement.textContent = age;
   }
 
   // Aggiornamento iniziale dell'età
